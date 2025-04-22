529d71dd5604549b394d1d6cfc601784f0a969dc
Laura <laura.van.dinteren@estudiantat.upc.edu>
2025-04-22 21:25:14 +0200

    feat: autenticacion tokens

--- lib/log_in.dart
+++ lib/log_in.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 
 
 void main() {
@@ -19,12 +23,122 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
+
+  Future<void> saveAccessToken(String access, String refresh) async {
+    await _secureStorage.write(key: 'access', value: access);
+    await _secureStorage.write(key: 'refresh', value: refresh);
+  }
+
+  Future<String?> getAccessToken() async {
+    return await _secureStorage.read(key: 'access');
+  }
+
+  Future<String?> getRefreshToken() async {
+    return await _secureStorage.read(key: 'refresh');
+  }
+
+
+
   @override
   void dispose() {
     _usernameController.dispose();
     _passwordController.dispose();
     super.dispose();
   }
+  Future<void> getToken() async {
+    final url = Uri.parse('http://10.0.2.2:8000/token/');
+
+    final Map<String, dynamic> data = {
+      'email': _usernameController.text,
+      'password': _passwordController.text,
+    };
+
+    print(data);
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(data),
+      );
+
+      final Map<String, dynamic> resp = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        print('Log in correcte');
+        print('retorna: ${response.body}');
+        saveAccessToken(resp['access'], resp['refresh']);
+        _getInfo(resp['access']);
+      } else if (response.statusCode == 401) {
+        String? r = await getRefreshToken();
+        getTokenRefresh(r!);
+      } else {
+        print('Error al fer login: ${response.statusCode} - ${response.body}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  Future<void> getTokenRefresh(String refresh) async {
+    final url = Uri.parse('http://10.0.2.2:8000/token/refresh/');
+
+    final Map<String, dynamic> data = {
+      'refresh': refresh,
+    };
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(data),
+      );
+
+      if (response.statusCode == 200) {
+        print('retorna refresh: ${response.body}');
+        final Map<String, dynamic> resp = json.decode(response.body);
+        _getInfo(resp['access']);
+      } else {
+        print('Failed to create reservation: ${response.statusCode} - ${response.body}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  Future<void> _getInfo(String token) async {
+    final url = Uri.parse('http://10.0.2.2:8000/me/');
+
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(utf8.decode(response.bodyBytes));
+        print('Les dades són: $data');
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Failed to load user info')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: ${e.toString()}')),
+      );
+    }
+  }
+
+
+
 
   Widget _buildTextField(
       String label,
@@ -82,7 +196,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 const SizedBox(height: 24),
                 ElevatedButton(
                   onPressed: () {
-                    // Implement login logic
+                    getToken();
                   },
                   style: ElevatedButton.styleFrom(
                     padding: const EdgeInsets.symmetric(vertical: 16),
